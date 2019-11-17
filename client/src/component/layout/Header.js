14786414e6a74e00859852cca0ea68673d68d9a9
Bharat <yadavbharat163@gmail.com>
2019-11-17 23:44:15 +0530

    logo

--- client/src/component/layout/Header.js
+++ client/src/component/layout/Header.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className='header_section'>
-      <div className='container'>
-        <div className='row'>
-          <div className='logo dropdown'>
-            <Link to='/' title='Service + - Packers and Movers'>
-              {/* <img
-                src={require('../../assets/pnm/images/logo.png')}
-                alt='logo'
-                style={{ width: '90px' }}
-                alt='Service+ - Packers and Movers'
-              /> */}
+    <div className="header_section">
+      <div className="container">
+        <div className="row">
+          <div className="logo dropdown">
+            <Link to="/" title="Service + - Packers and Movers">
+              <img
+                src={require("../../assets/pnm/images/logo.png")}
+                alt="logo"
+                style={{ width: "65px" }}
+                alt="Service+ - Packers and Movers"
+              />
             </Link>
           </div>
-          <div className='header_text hidden-xs hidden-sm hidden-md'>
-            <h1 className='hed_title'>All Types of Service Providers</h1>
+          <div className="header_text hidden-xs hidden-sm hidden-md">
+            <h1 className="hed_title">All Types of Service Providers</h1>
           </div>
-          <div className='header_call'>
-            <span className='phone_no'>+91-8689890905</span>
-            <a id='' className='header_btn hidden-xs'>
+          <div className="header_call">
+            <span className="phone_no">+91-8689890905</span>
+            <a id="" className="header_btn hidden-xs">
               Request a Call Back
             </a>
           </div>
